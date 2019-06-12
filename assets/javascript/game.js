69ee56fa323be8b2dd627436634340bbef9585a6
Yuka Nishijima <48897211+yukanishijima@users.noreply.github.com>
2019-06-12 12:07:28 -0400

    Add reset function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,5 @@
-var randomScore = "";
 var targetScore = "";
-var yourScore =0;
+var yourScore = 0;
 var wins = 0;
 var losses = 0;
 
@@ -9,76 +8,79 @@ var crystal2 = 0;
 var crystal3 = 0;
 var crystal4 = 0;
 
+
+//generate a random number for targetScore 19-120
+function generateTarget() {
+  targetScore = Math.floor(Math.random()*(121-19)+19);
+  $("#targetScore").text(targetScore);
+}
+
+//generate a random number for each crystal 1-12
+function generateCrystal() {
+  crystal1 = Math.floor(Math.random()*(13-1)+1);
+  crystal2 = Math.floor(Math.random()*(13-1)+1);
+  crystal3 = Math.floor(Math.random()*(13-1)+1);
+  crystal4 = Math.floor(Math.random()*(13-1)+1);
+
+  console.log("crystal1 = " + crystal1);
+  console.log("crystal2 = " + crystal2);
+  console.log("crystal3 = " + crystal3);
+  console.log("crystal4 = " + crystal4);
+}
+
+//reset the game
+function reset() {
+  yourScore = 0;
+  $("#yourScore").html(yourScore);
+  generateTarget();
+  generateCrystal();
+}
+
+//check win or loss
+function checkResult() {
+
+  //if your score = target score
+  if (yourScore == parseInt(targetScore)) {
+    wins = 1 + wins;
+    $("#wins").text(wins);
+    console.log("Wins: " + wins);
+    reset();
+  }  
+  //if your score exceeds target score
+  if (yourScore > parseInt(targetScore)) {
+    losses++;
+    $("#losses").text(losses);
+    console.log("Losses: " + losses);
+    reset();
+  }
+}
+
+//when the page is loaded,
 $(document).ready(function(){
 
-  //generate a random number for targetScore 19-120
-  function generateTarget() {
-    randomScore = Math.floor(Math.random()*(121-19)+19);
-    $("#targetScore").text(randomScore);
-  }
   generateTarget();
-
-  //generate a random number for each crystal 1-12
-  function generateCrystal() {
-    crystal1 = Math.floor(Math.random()*(13-1)+1);
-    crystal2 = Math.floor(Math.random()*(13-1)+1);
-    crystal3 = Math.floor(Math.random()*(13-1)+1);
-    crystal4 = Math.floor(Math.random()*(13-1)+1);
-
-    console.log(crystal1);
-    console.log(crystal2);
-    console.log(crystal3);
-    console.log(crystal4);
-  }
   generateCrystal();
 
   //event
   $("#button-1").click(function() {
     yourScore += crystal1;
     $("#yourScore").text(yourScore);
-    result();
+    checkResult();
   });
+
   $("#button-2").click(function() {
     yourScore += crystal2;
     $("#yourScore").text(yourScore);
-    result();
-    console.log(yourScore);
+    checkResult();
   });
   $("#button-3").click(function() {
     yourScore += crystal3;
     $("#yourScore").text(yourScore);
-    result();
-    console.log(yourScore);
+    checkResult();
   });
   $("#button-4").click(function() {
     yourScore += crystal4;
     $("#yourScore").text(yourScore);
-    result();
-    console.log(yourScore);
+    checkResult();
   });
-
 });
-
-
-function result() {
-  
-  var targetScore = $("#targetScore").text();
-
-  console.log("target score is " + parseInt(targetScore));
-  console.log("your score is " + yourScore);
-
-//if your score = target score
-  if (yourScore == parseInt(targetScore)) {
-    wins = 1 + wins;
-    $("#wins").text(wins);
-    console.log("Wins: " + wins);
-  }  
-
-//if your score exceeds target score
-  if (yourScore > parseInt(targetScore)) {
-    losses++;
-    $("#losses").text(losses);
-    console.log("Losses: " + losses);
-  }
-
-}
